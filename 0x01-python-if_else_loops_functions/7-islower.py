4faa9772c991ea7795f54f910cdb5d799666c5a5
kevkatam <kevinkipruto5077@gmail.com>
2023-08-09 06:06:15 -0700

    added 7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord('c') >= 96 and ord('c') <= 122:
+        return True
+    else:
+        return False
+    if ord('c') >= 65 and ord('c') <= 90:
+        return True
+    else:
+        return False
