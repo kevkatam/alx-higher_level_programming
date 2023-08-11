a6ecc2567625cc1d4fd3cf85ab214d8af25cef3a
kevkatam <kevinkipruto5077@gmail.com>
2023-08-11 05:36:53 -0700

    added 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,17 +5,18 @@ if __name__ == "__main__":
     if i != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv != '/':
+    sign = argv[2]
+    if sign != '+' and sign != '-' and sign != '*' and sign != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     from calculator_1 import add, sub, mul, div
     a = int(argv[1])
     b = int(argv[3])
-    if argv[2] == '+':
+    if sign == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif argv[2] == '-':
+    elif sign == '-':
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif argv[2] == '*':
+    elif sign == '*':
         print("{:d} - {:d} = {:d}".format(a, b, mul(a, b)))
     else:
         print("{:d} - {:d} = {:d}".format(a, b, div(a, b)))
