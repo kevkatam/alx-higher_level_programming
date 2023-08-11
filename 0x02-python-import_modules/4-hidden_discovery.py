e3ab4317e29be3b6ed69f653d119119b2028120e
kevkatam <kevinkipruto5077@gmail.com>
2023-08-11 05:14:56 -0700

    modifed 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import hidden_4
-   for i in dir(hidden_4):
-       if i[0] == '_' and i[1] == '_':
-           continue           
-       else:
-           print(i)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] == '_' and i[1] == '_':
+            continue
+        else:
+            print(i)
