2f85adebb557f4d4917c82e69dd7baa622c45593
kevkatam <kevinkipruto5077@gmail.com>
2023-08-11 05:43:33 -0700

    modified 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     elif sign == '-':
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif sign == '*':
-        print("{:d} - {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     else:
-        print("{:d} - {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
