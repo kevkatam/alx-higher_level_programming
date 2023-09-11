fe06219e684bc9e0b0f53c8a48d419f4026afdf7
kevkatam <kevinkipruto5077@gmail.com>
2023-09-11 03:17:09 -0700

    modified 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-module MyList that inherits from list prints the list, but sorted
-(ascending sort)
-"""
-
-
 class MyList(list):
+    """ class that inherits list class
+    Args:
+        list: class list
+    """
     def print_sorted(self):
-        """ method that prints the sorted list
-        """
+        """ method that prints the sorted list """
         sorted_l = self.copy()
         sorted_l.sort()
         print(sorted_l)
