f12f9de231028c3c62eb7842afefff646566cfe6
kevkatam <kevinkipruto5077@gmail.com>
2023-09-12 16:11:08 -0700

    added 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module that contains a function that reads stdin line by line and computes
+metrics
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function that appends a new line when astring is found
+    Args:
+        filename: name of file
+        search_string: string to search
+        new_string: string to append
+
+    """
+    result = []
+    with open(filename, mode="r", encoding="UTF-8") as my_file:
+        for line in my_file:
+            reult += [line]
+            if line.find(search_string) != -1:
+                result += [new_string]
+
+    with open(filename, mode="r", encoding="UTF-8") as my_file:
+        my_file.write("".join(result))
