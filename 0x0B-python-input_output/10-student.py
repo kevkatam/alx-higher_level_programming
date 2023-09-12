6241aa8db6ca2527daa0b5ff0701a798bdfbb79e
kevkatam <kevinkipruto5077@gmail.com>
2023-09-12 14:39:03 -0700

    added 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module that contain method that etrieves a dictionary
+representation of a Student instance
+"""
+
+
+class Student:
+    """ class that retrieves a dictionary representation of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ method that returns dictionary representation of a Student
+        instance
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+            my_dict = {}
+            for i in range(len(attrs)):
+                for j in obj:
+                    if attrs[i] == j:
+                        my_dict[j] = obj[j]
+            return my_dict
+        return obj
