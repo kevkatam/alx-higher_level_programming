e6e06c4e2b1ae4af803e8f7b8f1e117d634ca281
kevkatam <kevkatam@gmail.com>
2023-11-10 05:20:18 -0800

    adding 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module square that defines a square
+"""
+
+
+class Square:
+    """ class square that defines a square """
+
+    def __init__(self, size):
+        """ method that initializes th attribute size of the square
+
+        Args:
+            size: size of the square(int)
+        """
+        slef.__size = size
