b9b24d7101458d06e183d4f49c6d718c1c47e558
kevkatam <kevkatam@gmail.com>
2023-11-13 12:15:50 -0800

    adding 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myC = 'C is fun';
+const myPython = 'Python is cool';
+const myJavascript = 'JavaScript is amazing';
+
+console.log(myC + '\n' + myPython + '\n' + myJavascript);
