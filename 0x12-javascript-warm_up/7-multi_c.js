dc81072b702182e9850c1942b1af27141e6c20b4
kevkatam <kevkatam@gmail.com>
2023-11-14 00:57:45 -0800

    adding 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+if (Number.isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
