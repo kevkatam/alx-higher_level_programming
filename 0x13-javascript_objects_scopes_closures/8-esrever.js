7699565d836b5f9534a925b61efb2ba2bac988f9
kevkatam <kevkatam@gmail.com>
2023-11-15 01:27:36 -0800

    modifed 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = [];
-  let len = list.length - 1;
+  const newList = [];
+  const len = list.length - 1;
   let j = 0;
   for (let i = len; i >= 0; i--) {
     newList[j] = list[i];
