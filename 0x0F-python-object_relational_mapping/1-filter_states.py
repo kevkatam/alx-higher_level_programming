2fe60ebf896b341bf8246147e5d6f5e16b83cd90
kevkatam <kevkatam@gmail.com>
2023-11-18 01:22:14 -0800

    modified 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
                          port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name LIKE 'N%'\
-                 ORDER BY states.id;")
+                 WHERE CONVERT(`name` USING Latin1)\
+                 COLLATE Latin1_General_CS\
+                 LIKE 'N%';")
     states = cur.fetchall()
 
     for state in states:
