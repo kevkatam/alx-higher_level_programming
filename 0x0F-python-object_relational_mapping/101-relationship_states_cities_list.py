0e006d7bce99dafb204a015ff76e2eea2264a776
kevkatam <kevkatam@gmail.com>
2023-11-24 00:33:31 -0800

    modified 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,14 +4,14 @@ script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa
 """
 import sys
-from relationship_state import State, Base
+from relationship_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_city import City
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb:/{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
