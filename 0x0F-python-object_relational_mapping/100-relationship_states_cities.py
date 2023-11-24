73032270295c49112ee19f1ad41a78a9c7f30e58
kevkatam <kevkatam@gmail.com>
2023-11-23 23:44:11 -0800

    modified 100-relationship_states.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,7 +6,7 @@
 from sqlalchemy import create_engine
 import sys
 from relationship_city import City
-from relationship_state import State, Base
+from relationship_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 
