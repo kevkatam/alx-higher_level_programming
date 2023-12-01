302f43cc1bc2802b463360162b30ebaa4fe02835
kevkatam <kevkatam@gmail.com>
2023-12-01 09:01:26 -0800

    adding 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     body = r.text
     print("Body response:")
     print("\t- type: {}".format(type(body)))
-    print("\t-content: {}".format(body))
+    print("\t- content: {}".format(body))
