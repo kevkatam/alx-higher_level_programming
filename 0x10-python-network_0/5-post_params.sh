3ebdfaa939abfa66503a5f6977d0fb39137f8c73
kevkatam <kevkatam@gmail.com>
2023-12-05 00:57:18 -0800

    modified 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#cript that takes in a URL, sends a POST request to the passed URL, and
-#displays the body of the response
+#cript that takes in a URL, sends a POST request to the passed URL
 curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
