08ef95d1256ad968de6d163baa46ff9c0b12ebb2
kevkatam <kevkatam@gmail.com>
2024-01-26 06:28:29 -0800

    modified 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,11 +8,11 @@ request(argv[2], function (error, response, body) {
     const json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
       if (json[i].completed === true) {
-	if (res[json[i].userId] === undefined) {
-	  res[json[i].userId] = 0;
-	}
-	res[json[i].userId]++;
-     }
+        if (res[json[i].userId] === undefined) {
+          res[json[i].userId] = 0;
+        }
+        res[json[i].userId]++;
+      }
     }
     console.log(res);
   }
